bb3f28bd1bedcab9436aabf6dd81a0c459eb0a66
KeerthanaPrabhu04 <167683790+KeerthanaPrabhu04@users.noreply.github.com>
2024-05-09 19:00:07 +0530

    env.sv

---
+++ env.sv
@@ -0,0 +1,24 @@
+class env;
+  agent agt;
+  scoreboard sb;
+  mailbox mon_to_sb;
+
+  // Constructor
+  function new(virtual encoder_if vif); // Declare encoder_if as a virtual interface
+    mon_to_sb= new();
+    agt = new(vif,mon_to_sb);
+    sb=new(mon_to_sb);
+  endfunction
+
+  // Define environment tasks
+  task run();
+    fork
+      agt.run();
+      sb.run();
+    join_any
+    //wait(agt.gen.count == sb.compare_cnt);  
+    wait(sb.finished_processing);
+    #10;
+    $finish;
+  endtask
+endclass
